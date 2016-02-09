1eb74ad7213ca53b50d9f1b26ffa20fde72b73c6
gerard0315 <gerardtao0315@gmail.com>
2016-02-09 00:11:09 -0500

    AWS
    Database functions

--- boto_test.py
+++ boto_test.py
@@ -16,10 +16,10 @@ def add_item(new_item, target_table):
         print "Error adding item"
         return
 
-def delete_tem(item_to_delete, target_table):
+def delete_tem(target_table, feature0, feature1, feature2, feature3):
 	try:
         table = Table(target_table)
-        table.delete_item(item_to_delete)
+        table.delete_item(Sex = feature0, CUID = feature1, First_Name = feature2, Last_Name = feature3)
         #TODO!!!
         time.sleep(0.5)
         return
@@ -73,10 +73,20 @@ if __name__ == "__main__":
                                 target_table = command[3]
                                 print "adding item"
                                 add_item(new_item, target_table)
-                        elif command[0] == "delete" and len(command) == 4:
+                        elif command[0] == "delete" and len(command) == 4: 
                                 item_to_delete = command[1]
                                 target_table = command[3]
+                                print "please now input item details. "
+                                print "Sex:"
+                                input_0 = raw_input('>>')
+                                print "CUID":
+                                input_1 = raw_input('>>')
+                                print "First_Name:"
+                                input_2 = raw_input('>>')
+                                print "Last_Name:"
+                                input_3 = raw_input('>>')
                                 print "run deleting function"
+                                delete_item(target_table, input_0, input_1, input_2, input_3)
                         elif command[0] == "view" and len(command) == 2:
                                 target_table = command[1]
                                 view_table(target_table)
