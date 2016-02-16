d53b308bc68ab827f5039fa9e051fe93fc92b0f3
gerard0315 <gerardtao0315@gmail.com>
2016-02-16 16:20:52 -0500

    delete

--- Lab3/dynamodata.py
+++
@@ -1,4 +0,0 @@
-# *********************************************************************************************
-# Program to update dynamodb with latest data from mta feed. It also cleans up stale entried from db
-# Usage python dynamodata.py
-
